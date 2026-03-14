614aaa93e8767c15447a5d411fd5c2b7a1a15e52
divya <divyakabaddi03@gmail.com>
2026-03-14 15:49:49 +0530

    Add Cloudinary configuration

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
 
 app.use(cors({
     origin: '*',
